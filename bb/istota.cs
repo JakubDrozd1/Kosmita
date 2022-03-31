5ea4b6ff99df1272f6e97220e661858b1477a03c
Jakub Drózd <jakudro043@student.polsl.pl>
2022-03-31 11:05:38 +0200

    Add project files.

---
+++ bb/istota.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bb
+{
+    abstract class Istota
+    {
+        public string Opis;
+        public abstract string Inne();
+       
+
+        public virtual string Wyswietl()
+        {
+            return Opis + Inne();
+        }
+    }
+
+    class Kosmita : Istota
+    {
+
+        public string oko;
+
+        public Kosmita(string o)
+        {
+            oko = o;
+        }
+        public override string Inne()
+        {
+            return oko;
+        }
+
+
+
+    }
+    class Wąż : Istota
+    {
+        public string długość;
+
+        public Wąż(string d)
+        {
+            długość = d;
+        }
+        public override string Inne()
+        {
+            return długość;
+        }
+
+    }
+}
